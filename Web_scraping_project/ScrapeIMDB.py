b7e764523cfbdb9c42be739f39562ba1c208be0e
Mary Lilly Neenu Kurumparambil Adril <83948144+marylilly@users.noreply.github.com>
2022-02-01 19:28:17 +0100

    Add files via upload

---
+++ Web_scraping_project/ScrapeIMDB.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+try:
+    source=requests.get('https://www.imdb.com/chart/top/')
+    source.raise_for_status()
+
+    soup=BeautifulSoup(source.text,'html.parser')
+    movies=soup.find('tbody',class_="lister-list").find_all('tr')
+    for movie in movies:
+        name=movie.find('td',class_="titleColumn").a.text
+        rank=movie.find('td',class_="titleColumn").get_text(strip=True).split('.')[0]
+        year=movie.find('td',class_="titleColumn").span.text.strip('()')
+        rating=movie.find('td',class_="ratingColumn imdbRating").strong.text
+        print(rank,name,year,rating)
+except Exception as e:
+    print(e)
+
